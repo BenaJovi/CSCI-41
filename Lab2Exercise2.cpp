3d191a2d7b12b858aa27254b580a231983418325
Jovani Benavides <nickbenavides@Nicks-MacBook-Pro.local>
2023-09-01 20:49:56 -0700

    lab ex2 done

---
+++ Lab2Exercise2.cpp
@@ -0,0 +1,73 @@
+/*  Author: Jovani Benavides
+ *  Course: CSCI-41
+ *  Purpose: 
+ *  
+ */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Define a struct for 3D points
+struct Point {
+    double x, y, z;
+};
+
+// Function to calculate the distance between two points
+double distanceBetweenPoints(const Point& a, const Point& b) {
+    double dx = b.x - a.x;
+    double dy = b.y - a.y;
+    double dz = b.z - a.z;
+    return sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+// Function to calculate the distance between a point and the origin
+double distanceToOrigin(const Point& p) {
+    return sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
+}
+
+// Function to add two points
+Point addPoints(const Point& a, const Point& b) {
+    Point result;
+    result.x = a.x + b.x;
+    result.y = a.y + b.y;
+    result.z = a.z + b.z;
+    return result;
+}
+
+// Function to subtract two points
+Point subtractPoints(const Point& a, const Point& b) {
+    Point result;
+    result.x = a.x - b.x;
+    result.y = a.y - b.y;
+    result.z = a.z - b.z;
+    return result;
+}
+
+int main() {
+    Point pointA, pointB;
+
+    // Input coordinates for two points
+    cout << "Enter coordinates for Point A (x y z): ";
+    cin >> pointA.x >> pointA.y >> pointA.z;
+
+    cout << "Enter coordinates for Point B (x y z): ";
+    cin >> pointB.x >> pointB.y >> pointB.z;
+
+    // Calculate and display the distances
+    double distanceBetweenTwoPoints = distanceBetweenPoints(pointA, pointB);
+    double distanceToOrigin1 = distanceToOrigin(pointA);
+    double distanceToOrigin2 = distanceToOrigin(pointB);
+
+    cout << "Distance between Point A and Point B: " << distanceBetweenTwoPoints << endl;
+    cout << "Distance from Point A to the origin: " << distanceToOrigin1 << endl;
+    cout << "Distance from Point B to the origin: " << distanceToOrigin2 << endl;
+
+    // Calculate and display the result of adding and subtracting points
+    Point sum = addPoints(pointA, pointB);
+    Point difference = subtractPoints(pointA, pointB);
+
+    cout << "Sum of Point A and Point B (x y z): " << sum.x << " " << sum.y << " " << sum.z << endl;
+    cout << "Difference between Point A and Point B (x y z): " << difference.x << " " << difference.y << " " << difference.z << endl;
+
+    return 0;
+}
---
+++ Lab2Exercise3.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+class Point {
+private:
+    double x, y, z;
+
+public:
+    // Default Constructor
+    Point() : x(0.0), y(0.0), z(0.0) {}
+
+    // Constructor with 3 values
+    Point(double xVal, double yVal, double zVal) :
+     x(xVal), y(yVal), z(zVal) 
+     {
+
+     }
+
+    // Copy Constructor
+    Point(const Point& other) :
+     x(other.x), y(other.y), z(other.z) 
+     {
+
+     }
+
+    // Accessor functions
+    double getX() const { return x; }
+    double getY() const { return y; }
+    double getZ() const { return z; }
+
+    // Modifier functions
+    void setX(double xVal) { x = xVal; }
+    void setY(double yVal) { y = yVal; }
+    void setZ(double zVal) { z = zVal; }
+
+    // Function to calculate the distance between two points
+    double distanceBetweenPoints(const Point& other) const 
+    {
+        double dx = other.x - x;
+        double dy = other.y - y;
+        double dz = other.z - z;
+        return sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    // Function to calculate the distance between a point and the origin
+    double distanceToOrigin() const
+    {
+        return sqrt(x * x + y * y + z * z);
+    }
+
+    // Overload the + operator to add two points
+    Point operator+(const Point& other) 
+    const 
+    {
+        return Point(x + other.x, y + other.y, z + other.z);
+    }
+
+    // Overload the - operator to subtract two points
+    Point operator-(const Point& other) const 
+    {
+        return Point(x - other.x, y - other.y, z - other.z);
+    }
+};
+
+int main() 
+{
+    // Create Point objects using different constructors
+    Point a; // Default constructor
+    Point b(1.0, 2.0, 3.0); // Constructor with 3 values
+    Point c = b; // Copy constructor
+
+    // Access and modify point coordinates
+    a.setX(4.0);
+    a.setY(5.0);
+    a.setZ(6.0);
+
+    // Calculate and display distances
+    double distanceBetweenAAndB = a.distanceBetweenPoints(b);
+    double distanceToOriginC = c.distanceToOrigin();
+
+    cout << "Distance between a and b: " << distanceBetweenAAndB << endl;
+    cout << "Distance from c to the origin: " << distanceToOriginC << endl;
+
+    // Add and subtract points
+    Point sum = a + b;
+    Point difference = a - b;
+
+    cout << "Sum of a and b: (" << sum.getX() << ", " << sum.getY() << ", " << sum.getZ() << ")" << endl;
+    cout << "Difference between a and b: (" << difference.getX() << ", " << difference.getY() << ", " << difference.getZ() << ")" << endl;
+
+    return 0;
+}
