4874b85af6cc8f61a54525d463f932cba7ccbb0d
Jovani Benavides <nickbenavides@Nicks-MacBook-Pro.local>
2023-10-05 11:08:01 -0700

    lab7 commit

--- Lab7.cpp
+++ Lab7.cpp
@@ -1,36 +1,137 @@
 /*  Author: Jovani Benavides
  *  Course: CSCI-41
- *  
- *  
+ *
+ *
  */
 
-#include <iostream> 
+#include <iostream>
 using namespace std;
-
-
-
-
-
-
-// Print array function
-void printArray(int arr[], int n)
+// Quick Sort
+int partition(int arr[], int start, int end)
 {
-    for (int i=0; i < n; i++)
-        cout<<arr[i]<<" ";
-    cout<<"\n";
+    int pivot = arr[start];
+    int count = 0;
+
+    for (int i = start + 1; i <= end; ++i)
+    {
+        if (arr[i] <= pivot)
+            count++;
+    }
+
+    int pivotIndex = start + count;
+    swap(arr[pivotIndex], arr[start]);
+
+    int i = start;
+    int j = end;
+    while (i < pivotIndex && j > pivotIndex)
+    {
+
+        while (arr[i] <= pivot)
+        {
+            i++;
+        }
+
+        while (arr[j] > pivot)
+        {
+            j--;
+        }
+
+        if (i < pivotIndex && j > pivotIndex)
+        {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+    return pivotIndex;
+}
+// Quick Sort
+void quickSort(int arr[], int start, int end)
+{
+    if (start >= end)
+        return;
+    int p = partition(arr, start, end);
+
+    quickSort(arr, start, p - 1);
+
+    quickSort(arr, p + 1, end);
 }
 
+// Merge Sort
+void merge(int array[], int const left,int const mid, int const right)
+{
+    int const subArrayOne = mid - left + 1;
+    int const subArrayTwo = right - mid;
 
+    int *leftArray = new int[subArrayOne];
+    int *rightArray = new int[subArrayTwo];
+    for (int i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (int j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+    int indexOfSubArrayOne = 0;
+    int indexOfSubArrayTwo = 0;
+    int indexOfMergedArray = left;
+    while (indexOfSubArrayOne < subArrayOne &&
+           indexOfSubArrayTwo < subArrayTwo)
+    {
+        if (leftArray[indexOfSubArrayOne] <=rightArray[indexOfSubArrayTwo])
+        {
+            array[indexOfMergedArray]=leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else
+        {
+            array[indexOfMergedArray] =rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    while (indexOfSubArrayOne < subArrayOne)
+    {
+        array[indexOfMergedArray] =leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    while (indexOfSubArrayTwo < subArrayTwo)
+    {
+        array[indexOfMergedArray] =rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+void mergeSort(int array[],int const begin,int const end)
+{
+    if (begin >= end)
+        return;
+    int mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
 
-int main(){
-    int size=5;                                         //creates an a array with size of ten using randomly generated numbers
-   int arr[size];
-   cout<<"Array size is set to 10"<<endl;
-   for(int i=0;i<size;i++)
-    arr[i]=rand()%100; 
-   cout<<"Elements of the array:"<<endl;
-   for(int i=0;i<size;i++)
-   cout<<"Elements no "<<i+1<<":"<<arr[i]<<endl;
+int main()
+{
+    int size = 6; // creates an a array with size of ten using randomly generated numbers
+    int arr[size];
+    cout << "Array size is set to 6" << endl;
+    for (int i = 0; i < size; i++)
+        arr[i] = rand() % 100;
+    cout << "Elements of the array:" << endl;
+    for (int i = 0; i < size; i++)
+        cout << "Elements no " << i + 1 << ":" << arr[i] << endl;
 
-    int n = sizeof(arr)/sizeof(arr[0]); 
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n);                  // Using Quck Sort
+    cout<<"sorted using Quick Sort:"<<endl;
+    printArray(arr, n);
+
+    mergeSort(arr, 0, n - 1);             // Using Merge Sort
+    cout<<"Sorted using merge sort:"<<endl;
+    printArray(arr, n);
 }
